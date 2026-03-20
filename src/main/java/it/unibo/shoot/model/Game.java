dc723984b5c0b26662964058eba18a64f7c3c553
Veenya <veramurgia02@gmail.com>
2026-03-20 18:16:20 +0100

    Fixed Window

--- src/main/java/it/unibo/shoot/model/Game.java
+++ src/main/java/it/unibo/shoot/model/Game.java
@@ -2,10 +2,14 @@ package it.unibo.shoot.model;
 
 //import java.awt.Window;
 import java.awt.Canvas;
+import it.unibo.shoot.view.Window;
 
 public class Game extends Canvas implements Runnable {
-    public Game() {
 
+    private static final long serialVersionUID = 1L;
+
+    public Game() {
+        new Window(1000, 563, "ShOOt", this);
     }
 
     public void run() {
