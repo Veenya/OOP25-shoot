cf9c9dcd2e0198f6bc91726c035227549b8cf2f4
peter ibrahim <peteressam610@gmail.com>
2026-05-29 19:35:09 +0200

    Delete src/test/java directory

--- src/test/java/MainTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class MainTest {
-    @Test
-    void testWorks() {
-        assertTrue(true);
-    }
-}
