a2bb2516dd7b3fa46ced7b242c49aff853356dfb
peter ibrahim <peteressam610@gmail.com>
2026-06-25 16:14:48 +0200

    Add files via upload

---
+++ src/main/java/it/unibo/shoot/model/LevelManager.java
@@ -0,0 +1,85 @@
+package it.unibo.shoot.model;
+
+import java.util.*;
+import it.unibo.shoot.Upgrades.*;
+import it.unibo.shoot.audio.Sound;
+
+
+public class LevelManager {
+    private int currentLevel = 1;
+    private int currentXP = 0;
+    private int nextLevelXP = 100;
+    private long lastLevelUpTime = 0;
+    private List<Upgrade> availableUpgrades;
+    Game game;
+
+    public LevelManager(Game game) {
+        this.game = game;
+        this.availableUpgrades = new ArrayList<>();
+        
+        // Inizializzo il pool con i 5 upgrade che abbiamo creato
+        availableUpgrades.add(new SpeedUpgrade());
+        availableUpgrades.add(new HealthUpgrade());
+        availableUpgrades.add(new DamageUpgrade());
+        availableUpgrades.add(new EvasionUpgrade());
+    }
+
+    public void addXP(int amount) {
+        currentXP += amount;
+        if (currentXP >= nextLevelXP) {
+            levelUp();
+        }
+    }
+
+    private void levelUp() {
+        currentXP -= nextLevelXP;
+        currentLevel++;
+        nextLevelXP = (int) (nextLevelXP * 1.25);
+        game.getSound().play(Sound.SoundType.LEVEL_UP);
+        this.lastLevelUpTime = System.currentTimeMillis();
+        triggerLevelUpMenu();
+    }
+    public int getCurrentLevel() {
+        return this.currentLevel;}
+    private void triggerLevelUpMenu() {
+    //Game.levelUpTime = System.currentTimeMillis();
+    // 1. Extract 3 random upgrades
+    List<Upgrade> options = getRandomUpgrades(3);
+    
+    if (options.isEmpty()) {
+        System.out.println("Tutti gli upgrade sono già al massimo!");
+        game.setGameState(STATE.GAME);
+        return;
+    }
+
+    // 2. Passa le opzioni estratte alla classe Game per renderizzarle a schermo
+        //Game.currentUpgradeOptions = options;
+        game.setUpgradeOptions(options);
+        
+        // 3. Cambia lo stato per congelare la fisica di nemici/spawner ed aprire il menu overlay
+        game.setGameState(STATE.LEVEL_UP);
+}
+
+    public List<Upgrade> getRandomUpgrades(int count) {
+        List<Upgrade> eligible = new ArrayList<>(availableUpgrades.stream()
+                .filter(u -> !u.isMaxed())
+                .toList());
+        
+        Collections.shuffle(eligible);
+        return eligible.subList(0, Math.min(count, eligible.size()));
+    }
+
+    public void setPlayer(Player player){
+    }
+
+    public int getCurrentXP() {
+        return this.currentXP;
+    }
+
+    public int getNextLevelXP() {
+        return this.nextLevelXP;
+    }
+    public long getLastLevelUpTime() {
+    return this.lastLevelUpTime;
+    }
+}
